023d128cddcac4362e4ba5399407a3012d9d021e
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-15 09:03:43 +0200

    Primo esempio

---
+++ Threading/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Threading
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Task_Click(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i <= 10000; i++)
+            {
+                for (int j = 0; j <= 100000; j++)
+                {
+
+                }
+            }
+            lbl_Risultato.Content = "finito";
+        }
+    }
+}
